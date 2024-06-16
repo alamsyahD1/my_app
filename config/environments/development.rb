3c1192ab7ab6b4537238c9aa191c8301ca178ff9
alamsyahD1 <alamsyah.2021@student.uny.ac.id>
2024-06-16 08:22:27 +0000

    first comit

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,5 +59,7 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
-  config.hosts << "musical-umbrella-q764w7rj529j7r-3000.app.github.dev"
+  config.hosts << "special-space-engine-q799wrwg6vrjh9q75-3000.app.github.dev"
 end
+  
+
